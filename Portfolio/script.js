0e58ee7a3db58b31a3c7b71c9496d1573c7159f0
Amisha <21j03.amisha@sjec.ac.in>
2023-12-13 21:55:18 +0530

    final commit

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,115 +1,117 @@
-let words=document.querySelectorAll(".word");
-words.forEach((word)=>{
-    let letters=word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
-        let span=document.createElement("span");
-        span.textContent =letter;
-        span.className="letter";
+// Split each word into letters and create span elements for each letter
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
         word.append(span);
     });
 });
 
-let currentWordIndex=0;
-let maxWordIndex = words.length-1;
-words[currentWordIndex].computedStyleMap.opacity="1";
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].computedStyleMap.opacity = "1";
 
-let changeText = ()=>{
-    let currentWord=words[currentWordIndex];
+// Function to change text in a cyclic manner
+let changeText = () => {
+    let currentWord = words[currentWordIndex];
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
-            letter.className="letter out";
-        },i*80);
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
 
-        });
-        nextWord.style.opacity="1";
-        Array.from(nextWord.children).forEach((letter,i)=>{
-            letter.className= "letter behind";
-            setTimeout(()=>{
-                letter.className="letter in";
-            },340+i*80);
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
 
-        });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
 
-currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex +1;
-    };
-
+// Initial text change and interval for cyclic text change
 changeText();
-setInterval(changeText,3000)
+setInterval(changeText, 3000);
 
+// Rendering circles with marked points based on data attributes
 const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=>{
+circles.forEach(elem => {
     var dots = elem.getAttribute("data-dots");
     var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
-    var points="";
+    var percent = Math.floor(dots * marked / 100);
+    var points = "";
     var rotate = 360 / dots;
 
-    for(let i=0;i<dots;i++)
-    {
+    for (let i = 0; i < dots; i++) {
         points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
     }
-    elem.innerHTML =points;
+    elem.innerHTML = points;
     const pointsMarked = elem.querySelectorAll('.points');
-    for(let i=0; i<percent;i++){
-        pointsMarked[i].classList.add('marked')
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked');
     }
-})
+});
 
-
+// Highlighting active menu item based on scrolling
 let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
 
-function activeMenu(){
+function activeMenu() {
     let len = section.length;
-    while(--len && window.scrollY +97 < section[len].ofsetTop){}
-    menuLi.forEach(sec=> sec.classList.remove("active"));
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { }
+    menuLi.forEach(sec => sec.classList.remove("active"));
     menuLi[len].classList.add("active");
-
 }
 
 activeMenu();
 window.addEventListener("scroll", activeMenu);
 
+// Sticky header on scroll
 const header = document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY>50)
-})
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 50);
+});
 
+// Toggle mobile navigation menu
 let menuIcon = document.querySelector("#menu-icon");
-let navlist=document.querySelector(".navlist");
+let navlist = document.querySelector(".navlist");
 
-menuIcon.onclick= ()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navlist.classList.toggle("open");
 }
 
-window.onscroll = ()=>{
+// Close mobile navigation menu on scroll
+window.onscroll = () => {
     menuIcon.classList.remove("bx-x");
     navlist.classList.remove("open");
 }
 
-const observer = new InteractionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
+// Intersection Observer for fading in elements on scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show-items");
+        } else {
+            entry.target.classList.remove("show-items");
         }
-        else{
-            entry.target.classList.remove("shoe-Items");
-        }
-        
     });
-
 });
 
-
+// Apply Intersection Observer to elements with specific classes
 const scrollScale = document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el));
+scrollScale.forEach((el) => observer.observe(el));
 
 const scrollBottom = document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el));
+scrollBottom.forEach((el) => observer.observe(el));
 
 const scrollTop = document.querySelectorAll(".scroll-scale");
-scrollTop.forEach((el)=>observer.observe(el));
+scrollTop.forEach((el) => observer.observe(el));
--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,56 +1,82 @@
+// Get the reference to the list container and input box
 const listContainer = document.getElementById("list-container");
 const inputBox = document.getElementById("input-box");
 
+// Function to add a new task
 function addTask() {
+    // Check if the input box is empty
     if (inputBox.value === '') {
         alert("Enter Some Data");
     } else {
+        // Create a new list item and set its content
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        // Append the list item to the list container
         listContainer.appendChild(li);
 
+        // Create a span element for the delete (x) button
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        // Append the span to the list item
         li.appendChild(span);
 
-       let editButton = document.createElement("button");
-       editButton.textContent =" ";
-       editButton.classList.add("edit-button"); 
-       li.appendChild(editButton);
+        // Create an edit button
+        let editButton = document.createElement("button");
+        editButton.textContent = " ";
+        editButton.classList.add("edit-button");
+        // Append the edit button to the list item
+        li.appendChild(editButton);
     }
 
+    // Clear the input box
     inputBox.value = "";
+    // Save the updated task list
     saveTask();
 }
 
+// Event listener for clicks on the list container
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
+        // Toggle the 'checked' class for list items
         e.target.classList.toggle("checked");
+        // Save the updated task list
         saveTask();
     } else if (e.target.tagName === "SPAN") {
+        // Remove the parent list item when the delete button is clicked
         e.target.parentElement.remove();
+        // Save the updated task list
         saveTask();
     } else if (e.target.tagName === "BUTTON" && e.target.textContent === " ") {
+        // Call the editTask function when the edit button is clicked
         editTask(e.target.parentElement);
     }
 });
 
+// Function to edit a task
 function editTask(liElement) {
+    // Get the original text content of the list item
     const originalText = liElement.firstChild.textContent;
+    // Prompt the user to enter a new text
     const newText = prompt("Edit task:", originalText);
 
-    if (newText !== null) { // Check if the user didn't cancel the edit
+    // Check if the user didn't cancel the edit
+    if (newText !== null) {
+        // Set the new text content for the list item
         liElement.firstChild.textContent = newText;
+        // Save the updated task list
         saveTask();
     }
 }
 
+// Function to save the task list to local storage
 function saveTask() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
+// Function to show the tasks from local storage on page load
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
+// Call the showTask function to display tasks on page load
 showTask();
